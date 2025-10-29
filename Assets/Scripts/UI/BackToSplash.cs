15adeede6b7165b135f0cebb94c575261abf848d
Vitalii Ponomarenko <91014003+Minatoeh@users.noreply.github.com>
2025-10-29 23:31:49 +0200

    work with dots

--- Assets/Scripts/UI/BackToSplash.cs
+++ Assets/Scripts/UI/BackToSplash.cs
@@ -1,39 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BackToSplash : MonoBehaviour
 {
-
-    public string sceneToLoad;
-    private GameData gameData;
-    private Board board;
+    [SerializeField] private string splashSceneName = "Splash";
 
     public void WinOk()
     {
-        if(gameData != null)
+        var board = FindObjectOfType<Board>();
+        var scoreMgr = FindObjectOfType<ScoreManager>();
+        var gameData = FindObjectOfType<GameData>();
+
+        int level = board != null ? Mathf.Max(0, board.level) : 0;
+        int score = scoreMgr != null ? scoreMgr.score : 0;
+
+        if (gameData != null && gameData.saveData != null)
         {
-            gameData.saveData.isActive[board.level + 1] = true;
+            var sd = gameData.saveData;
+
+            if (sd.highScores != null && level < sd.highScores.Length)
+                sd.highScores[level] = Mathf.Max(sd.highScores[level], score);
+
+            if (sd.stars != null && level < sd.stars.Length && board != null && board.scoreGoals != null)
+                sd.stars[level] = Mathf.Max(sd.stars[level], CalcStars(score, board.scoreGoals));
+
             gameData.Save();
         }
-        SceneManager.LoadScene(sceneToLoad);
+
+        int nextLevel = level + 1;
+        if (board != null && board.world != null && board.world.levels != null && nextLevel < board.world.levels.Length)
+        {
+            PlayerPrefs.SetInt("Current Level", nextLevel);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene(splashSceneName);
+        }
     }
 
-    public void LoseOk()
+    private int CalcStars(int score, int[] goals)
     {
-        SceneManager.LoadScene(sceneToLoad);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameData = FindObjectOfType<GameData>();
-        board = FindObjectOfType<Board>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (goals == null || goals.Length == 0) return 0;
+        int stars = 0;
+        for (int i = 0; i < goals.Length; i++)
+            if (score >= goals[i]) stars = i + 1;
+        return stars;
     }
 }
